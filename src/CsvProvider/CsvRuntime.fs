beff726007c21b652d4f2d1d366ddd170a6ebf76
Gustavo Guerra <gustavo@codebeside.org>
2013-01-29 22:21:08 +0000

    Improve error messages

--- src/CsvProvider/CsvRuntime.fs
+++ src/CsvProvider/CsvRuntime.fs
@@ -61,6 +61,10 @@ module internal CsvReader =
 [<StructuredFormatDisplay("{Display}")>]
 type CsvRow internal (data:string[], headers:string[]) =
 
+  do 
+    if data.Length <> headers.Length then
+      failwithf "Invalid CSV row: expected %d columns, but got %d: %A" headers.Length data.Length (data |> Seq.map (fun s -> "\"" + s + "\"") |> String.concat " ")
+
   /// The raw data
   member x.Columns = data
 
@@ -88,7 +92,13 @@ type CsvFile (reader:TextReader, ?sep:string) =
 
   /// Read the input and cache it (we can read input only once)
   let file = CsvReader.readCsvFile reader (sep.ToCharArray()) |> Seq.cache
+
+  do 
+    if Seq.isEmpty file then
+      failwithf "Invalid CSV file: header row not found" 
+
   let headers = file |> Seq.head
+
   let data = file |> Seq.skip 1 |> Seq.map (fun v -> CsvRow(v, headers)) |> Seq.cache
 
   member x.Data = data
