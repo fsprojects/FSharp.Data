e9e2228e5baaa1566c7f13a809d21d418a190cfa
Tomas Petricek <tomas@tomasp.net>
2013-01-29 03:07:56 +0000

    Use consistent indentation in CSV provider code

--- src/CsvProvider/CsvGenerator.fs
+++ src/CsvProvider/CsvGenerator.fs
@@ -11,22 +11,22 @@ open ProviderImplementation.ProvidedTypes
 
 module internal CsvTypeBuilder =
 
-    let generateCsvRowType culture (replacer:AssemblyReplacer) (parentType:ProvidedTypeDefinition) fields = 
+  let generateCsvRowType culture (replacer:AssemblyReplacer) (parentType:ProvidedTypeDefinition) fields = 
 
-        let objectTy = ProvidedTypeDefinition("Row", Some(replacer.ToRuntime typeof<CsvRow>), HideObjectMethods = true)
-        parentType.AddMember objectTy
+    let objectTy = ProvidedTypeDefinition("Row", Some(replacer.ToRuntime typeof<CsvRow>), HideObjectMethods = true)
+    parentType.AddMember objectTy
 
-        for index, field in fields |> Seq.mapi (fun i v -> i, v) do
-            let typ, typWithMeasure =
-                match field.Type with
-                | Primitive(typ, Some unit) -> 
-                    typ, ProvidedMeasureBuilder.Default.AnnotateType(typ, [unit])
-                | Primitive(typ, None) -> typ, typ
-                | _ -> typeof<string>, typeof<string>
+    for index, field in fields |> Seq.mapi (fun i v -> i, v) do
+      let typ, typWithMeasure =
+        match field.Type with
+        | Primitive(typ, Some unit) -> 
+            typ, ProvidedMeasureBuilder.Default.AnnotateType(typ, [unit])
+        | Primitive(typ, None) -> typ, typ
+        | _ -> typeof<string>, typeof<string>
 
-            let typ, conv = Conversions.convertValue culture field.Name (if field.Optional then TypeWrapper.Nullable else TypeWrapper.None) (typ, typWithMeasure) replacer
-            let p = ProvidedProperty(NameUtils.nicePascalName field.Name, typ)
-            p.GetterCode <- fun (Singleton row) -> let row = replacer.ToDesignTime row in conv <@@ Operations.AsOption((%%row:CsvRow).Columns.[index]) @@>
-            objectTy.AddMember p
+      let typ, conv = Conversions.convertValue culture field.Name (if field.Optional then TypeWrapper.Nullable else TypeWrapper.None) (typ, typWithMeasure) replacer
+      let p = ProvidedProperty(NameUtils.nicePascalName field.Name, typ)
+      p.GetterCode <- fun (Singleton row) -> let row = replacer.ToDesignTime row in conv <@@ Operations.AsOption((%%row:CsvRow).Columns.[index]) @@>
+      objectTy.AddMember p
 
-        objectTy
+    objectTy
--- src/CsvProvider/CsvInference.fs
+++ src/CsvProvider/CsvInference.fs
@@ -14,30 +14,30 @@ open ProviderImplementation.StructureInference
 /// (This handles units in the same way as the original MiniCSV provider)
 let inferFields (csv:CsvFile) count culture =
   
-    // Infer the units and names from the headers
-    let headers = csv.Headers |> Seq.map (fun header ->
-        let m = Regex.Match(header, @"(?<field>.+) \((?<unit>.+)\)")
-        if m.Success then
-            let headerName = m.Groups.["field"].Value
-            let unitName = m.Groups.["unit"].Value
-            Some(ProvidedMeasureBuilder.Default.SI unitName), headerName
-        else None, header)
+  // Infer the units and names from the headers
+  let headers = csv.Headers |> Seq.map (fun header ->
+    let m = Regex.Match(header, @"(?<field>.+) \((?<unit>.+)\)")
+    if m.Success then
+      let headerName = m.Groups.["field"].Value
+      let unitName = m.Groups.["unit"].Value
+      Some(ProvidedMeasureBuilder.Default.SI unitName), headerName
+    else None, header)
   
-    // Infer the type of collection using structural inference
-    let types = seq {
-        for row in Seq.truncate count csv.Data ->
-            let fields = 
-                [ for (unit, header), value in Seq.zip headers row.Columns ->
-                    let typ = inferPrimitiveType culture value unit
-                    { Name = header; Optional = false; Type = typ } ]
-            Record(None, fields) }
+  // Infer the type of collection using structural inference
+  let types = seq {
+    for row in Seq.truncate count csv.Data ->
+      let fields = 
+        [ for (unit, header), value in Seq.zip headers row.Columns ->
+            let typ = inferPrimitiveType culture value unit
+            { Name = header; Optional = false; Type = typ } ]
+      Record(None, fields) }
  
-    let typ = Seq.reduce subtypeInfered types  
+  let typ = Seq.reduce subtypeInfered types  
    
-    let convertOptionalDecimalToFloat = function
-    | { Name = name; Optional = true; Type = Primitive(t, unit) } when t = typeof<decimal> -> { Name = name; Optional = false; Type = Primitive(typeof<float>, unit) }
-    | p -> p
+  let convertOptionalDecimalToFloat = function
+  | { Name = name; Optional = true; Type = Primitive(t, unit) } when t = typeof<decimal> -> { Name = name; Optional = false; Type = Primitive(typeof<float>, unit) }
+  | p -> p
 
-    match typ with
-    | Record(_, fields) -> List.map convertOptionalDecimalToFloat fields
-    | _ -> failwith "generateCsvRowType: Type inference returned wrong type"
+  match typ with
+  | Record(_, fields) -> List.map convertOptionalDecimalToFloat fields
+  | _ -> failwith "generateCsvRowType: Type inference returned wrong type"
