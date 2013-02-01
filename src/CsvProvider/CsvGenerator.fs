ce0199641361d41018d2f98fb34f6b9b7c15b601
Gustavo Guerra <gustavo@codebeside.org>
2013-02-01 21:14:31 +0000

    Performance Improvements (#53)

--- src/CsvProvider/CsvGenerator.fs
+++ src/CsvProvider/CsvGenerator.fs
@@ -11,22 +11,29 @@ open ProviderImplementation.ProvidedTypes
 
 module internal CsvTypeBuilder =
 
-    let generateCsvRowType culture (replacer:AssemblyReplacer) (parentType:ProvidedTypeDefinition) fields = 
+  let generateCsvRowProperties culture (replacer:AssemblyReplacer) fields = 
 
-        let objectTy = ProvidedTypeDefinition("Row", Some(replacer.ToRuntime typeof<CsvRow>), HideObjectMethods = true)
-        parentType.AddMember objectTy
+    fields
+    |> List.mapi (fun index field ->
 
-        for index, field in fields |> Seq.mapi (fun i v -> i, v) do
-            let typ, typWithMeasure =
-                match field.Type with
-                | Primitive(typ, Some unit) -> 
-                    typ, ProvidedMeasureBuilder.Default.AnnotateType(typ, [unit])
-                | Primitive(typ, None) -> typ, typ
-                | _ -> typeof<string>, typeof<string>
+      let typ, typWithMeasure =
+        match field.Type with
+        | Primitive(typ, Some unit) -> 
+            typ, ProvidedMeasureBuilder.Default.AnnotateType(typ, [unit])
+        | Primitive(typ, None) -> typ, typ
+        | _ -> typeof<string>, typeof<string>
 
-            let typ, conv = Conversions.convertValue culture field.Name (if field.Optional then TypeWrapper.Nullable else TypeWrapper.None) (typ, typWithMeasure) replacer
-            let p = ProvidedProperty(NameUtils.nicePascalName field.Name, typ)
-            p.GetterCode <- fun (Singleton row) -> let row = replacer.ToDesignTime row in conv <@@ Operations.AsOption((%%row:CsvRow).Columns.[index]) @@>
-            objectTy.AddMember p
+      let typeWrapper = 
+        if field.Optional then 
+            TypeWrapper.Nullable 
+        else 
+            TypeWrapper.None
 
-        objectTy
+      let typ, conv = Conversions.convertValue culture field.Name typeWrapper (typ, typWithMeasure)  replacer
+      
+      let p = ProvidedProperty(NameUtils.nicePascalName field.Name, typ)
+      p.GetterCode <- fun (Singleton row) -> 
+        let row = replacer.ToDesignTime row 
+        conv <@@ Operations.AsOption((%%row:CsvRow).Columns.[index]) @@>
+      
+      p)
