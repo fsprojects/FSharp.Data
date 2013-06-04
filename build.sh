7351c122aa7df95aa057e7631d4390a842b67df0
Don Syme <donsyme@fastmail.fm>
2013-06-04 13:04:15 +0100

    add commands for running unit tests to build.sh

--- build.sh
+++ build.sh
@@ -18,4 +18,18 @@ xbuild  src/FSharp.Data.Experimental.DesignTime.fsproj   /p:Configuration="Relea
 
 xbuild FSharp.Data.Tests.sln /p:Configuration="Release" 
 
+# Note, NUnit.Runners 2.6.2 has a bug related to Mono, see https://bugs.launchpad.net/nunitv2/+bug/1076932. Version 2.6.1 fares better
+mono tools/NuGet/NuGet.exe install NUnit.Runners -Version 2.6.1
+
+# Note, these main tests are running fairly cleanly on Mac/Linux when last checked
+mono ./NUnit.Runners.2.6.1/tools/nunit-console.exe tests/FSharp.Data.Tests/bin/Release/FSharp.Data.Tests.dll -result=TestResult.FSharp.Data.Tests.xml
+
+# Note, these tests are NOT running cleanly on Mac/Linux when last checked
+# mono ./NUnit.Runners.2.6.1/tools/nunit-console.exe tests/FSharp.Data.Tests.DesignTime/bin/Release/FSharp.Data.Tests.DesignTime.dll  -result=TestResult.FSharp.Data.Tests.DesignTime.xml
+
+# Note, these tests are NOT running cleanly on Mac/Linux when last checked
+# mono ./NUnit.Runners.2.6.1/tools/nunit-console.exe  tests/FSharp.Data.Tests.Experimental.DesignTime/bin/Release/FSharp.Data.Tests.Experimental.DesignTime.dll -result=TestResult.FSharp.Data.Tests.Experimental.DesignTime.xml
+
+
+
 
