f3dec16f72125507539832db0a407f03706c36d7
Gustavo Guerra <gustavo@codebeside.org>
2013-10-11 15:00:50 +0100

    Remove .AsTuple

--- src/CsvProvider/CsvGenerator.fs
+++ src/CsvProvider/CsvGenerator.fs
@@ -34,9 +34,6 @@ module internal CsvTypeBuilder =
     
     let rowType = ProvidedTypeDefinition("Row", Some rowErasedType, HideObjectMethods = true)
     
-    // Allow to access the underlying representation
-    rowType.AddMember <| ProvidedProperty("AsTuple", rowErasedType, GetterCode = fun (Singleton row) -> row)
-
     // Each property of the generated row type will simply be a tuple get
     for field in fields do
       rowType.AddMember field.Property
