6d4098989d5b2ec7bd6388a6de3a1d51d913ad53
Gustavo Guerra <gustavo@codebeside.org>
2014-01-23 21:53:15 +0000

    Fix JavaScriptStringEncode (closes #200)

--- tests/FSharp.Data.Tests/HttpUtils.fs
+++ tests/FSharp.Data.Tests/HttpUtils.fs
@@ -2,12 +2,12 @@
 // Tests for Http Utilities (mainly HttpUtility.JavaScriptStringEncode now)
 // --------------------------------------------------------------------------------------
 
-module FSharp.Data.Tests.HttpUtility
-
 #if INTERACTIVE
 #r "../../bin/FSharp.Data.dll"
 #r "../../packages/NUnit.2.6.3/lib/nunit.framework.dll"
 #load "../Common/FsUnit.fs"
+#else
+module FSharp.Data.Tests.HttpUtility
 #endif
 
 open FsUnit
