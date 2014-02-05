d3a271ae58380103bf7106b8e6e467ffe754b7c0
Gustavo Guerra <gustavo@codebeside.org>
2014-02-05 15:57:50 +0000

    Update teamcity.cmd

--- teamcity.cmd
+++ teamcity.cmd
@@ -1,4 +1,4 @@
-call build CleanInternetCaches || exit /B %ERRORLEVEL%
-call build All || exit /B %ERRORLEVEL%
-call build SourceLink || exit /B %ERRORLEVEL%
-call build NuGet || exit /B %ERRORLEVEL%
+call build CleanInternetCaches || exit /B 1
+call build All || exit /B 1
+call build SourceLink || exit /B 1
+call build NuGet || exit /B 1
