9e0e67c691c3059a2c0fd0661ad851d40d90b167
Gustavo Guerra <gustavo@codebeside.org>
2014-02-08 20:52:58 +0000

    Fixes #348
    The problem was that after a version of FSharp.Data.dll was loaded, it's not possible to load another one. When we had only one profile it was fine, because full .Net was loaded normally, and the profile was loaded with reflection only, so there was no clash.
    I turned all framework version to reflection only load, and gave different version numbers to all assemblies to avoid this problem.

--- src/AssemblyInfo.Portable7.fs
+++ src/AssemblyInfo.Portable7.fs
@@ -1,12 +1,12 @@
 ﻿namespace System
 open System.Reflection
 
-[<assembly: AssemblyTitleAttribute("FSharp.Data.DesignTime")>]
+[<assembly: AssemblyTitleAttribute("FSharp.Data")>]
 [<assembly: AssemblyProductAttribute("FSharp.Data")>]
 [<assembly: AssemblyDescriptionAttribute("Library of F# type providers and data access tools")>]
-[<assembly: AssemblyVersionAttribute("2.0.0.0")>]
-[<assembly: AssemblyFileVersionAttribute("2.0.0.0")>]
+[<assembly: AssemblyVersionAttribute("2.0.0.7")>]
+[<assembly: AssemblyFileVersionAttribute("2.0.0.7")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.0.0.0"
+    let [<Literal>] Version = "2.0.0.7"
