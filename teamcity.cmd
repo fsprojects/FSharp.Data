fe82690228e7a11ec2c6e1aa3b2ef0cc194af546
Gustavo Guerra <gustavo@codebeside.org>
2014-02-09 18:03:19 +0000

    Update teamcity.cmd

--- teamcity.cmd
+++ teamcity.cmd
@@ -1,4 +1,3 @@
-git clone -config core.autocrlf=false https://github.com/fsharp/FSharp.Data.git
 call build CleanInternetCaches || exit /B 1
 call build All || exit /B 1
 call build SourceLink || exit /B 1
