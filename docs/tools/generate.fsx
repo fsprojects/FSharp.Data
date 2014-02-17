e622b207599a3df182fec4a6662cc809b09bb190
Gustavo Guerra <gustavo@codebeside.org>
2014-02-17 18:58:40 +0000

    Update FSharp.Formatting

--- docs/tools/generate.fsx
+++ docs/tools/generate.fsx
@@ -21,8 +21,8 @@ let info =
 // For typical project, no changes are needed below
 // --------------------------------------------------------------------------------------
 
-#I "../../packages/FSharp.Compiler.Service.0.0.15/lib/net40"
-#I "../../packages/FSharp.Formatting.2.3.7-beta/lib/net40"
+#I "../../packages/FSharp.Compiler.Service.0.0.20/lib/net40"
+#I "../../packages/FSharp.Formatting.2.3.10-beta/lib/net40"
 #I "../../packages/RazorEngine.3.3.0/lib/net40/"
 #r "../../packages/Microsoft.AspNet.Razor.2.0.30506.0/lib/net40/System.Web.Razor.dll"
 #r "../../packages/FAKE/tools/FakeLib.dll"
@@ -51,7 +51,7 @@ let output     = __SOURCE_DIRECTORY__ @@ "../output"
 let files      = __SOURCE_DIRECTORY__ @@ "../files"
 let data       = __SOURCE_DIRECTORY__ @@ "../content/data"
 let templates  = __SOURCE_DIRECTORY__ @@ "templates"
-let formatting = __SOURCE_DIRECTORY__ @@ "../../packages/FSharp.Formatting.2.3.7-beta/"
+let formatting = __SOURCE_DIRECTORY__ @@ "../../packages/FSharp.Formatting.2.3.10-beta/"
 let docTemplate = formatting @@ "templates/docpage.cshtml"
 
 // Where to look for *.cshtml templates (in this order)
