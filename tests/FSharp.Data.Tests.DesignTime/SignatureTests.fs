a132a29703091f1e17d461e5dd32c34969c1a401
Gustavo Guerra <gustavo@codebeside.org>
2014-02-24 21:10:56 +0000

    Fix UTs

--- tests/FSharp.Data.Tests.DesignTime/SignatureTests.fs
+++ tests/FSharp.Data.Tests.DesignTime/SignatureTests.fs
@@ -34,9 +34,6 @@ let sourceDirectory = __SOURCE_DIRECTORY__
 let testCases = 
     sourceDirectory ++ "SignatureTestCases.config" 
     |> File.ReadAllLines
-#if TEAM_CITY
-    |> Array.filter (fun x -> not (x.StartsWith "Freebase"))
-#endif
     |> Array.map TypeProviderInstantiation.Parse
 
 let expectedDirectory = sourceDirectory ++ "expected" 
