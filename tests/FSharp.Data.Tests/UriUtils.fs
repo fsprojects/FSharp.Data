2e33ef92129825cf5f3abd1f4419a0bbf3f54554
Gustavo Guerra <gustavo@codebeside.org>
2014-03-02 19:47:51 +0000

    Disable uri tests on mono

--- tests/FSharp.Data.Tests/UriUtils.fs
+++ tests/FSharp.Data.Tests/UriUtils.fs
@@ -18,25 +18,31 @@ open FSharp.Data.Runtime
 let uri = new Uri("http://www.myapi.com/%2F?Foo=Bar%2F#frag") |> UriUtils.enableUriSlashes
 
 [<Test>]
+[<Platform("Net")>]
 let ``ToString contains escaped slashes`` () =
     uri.ToString() |> should equal "http://www.myapi.com/%2F?Foo=Bar%2F#frag"
 
 [<Test>]
+[<Platform("Net")>]
 let ``AbsoluteUri contains escaped slashes`` () =
     uri.AbsoluteUri |> should equal "http://www.myapi.com/%2F?Foo=Bar%2F#frag"
 
 [<Test>]
+[<Platform("Net")>]
 let ``Query contains escaped slashes`` () =
     uri.Query |> should equal "?Foo=Bar%2F"
 
 [<Test>]
+[<Platform("Net")>]
 let ``PathAndQuery contains escaped slashes`` () =
     uri.PathAndQuery |> should equal "/%2F?Foo=Bar%2F"
 
 [<Test>]
+[<Platform("Net")>]
 let ``AbsolutePath contains escaped slashes`` () =
     uri.AbsolutePath |> should equal "/%2F"
 
 [<Test>]
+[<Platform("Net")>]
 let ``Uri Fragment is properly set`` () = 
     uri.Fragment |> should equal "#frag"
