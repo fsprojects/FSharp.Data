d48b66e536cda8e9d9ce8ef38609606646550f74
Gustavo Guerra <gustavo@codebeside.org>
2014-03-16 23:25:41 +0000

    Release 2.0.2

--- src/AssemblyInfo.Portable7.fs
+++ src/AssemblyInfo.Portable7.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Data")>]
 [<assembly: AssemblyProductAttribute("FSharp.Data")>]
 [<assembly: AssemblyDescriptionAttribute("Library of F# type providers and data access tools")>]
-[<assembly: AssemblyVersionAttribute("2.0.1.7")>]
-[<assembly: AssemblyFileVersionAttribute("2.0.1.7")>]
+[<assembly: AssemblyVersionAttribute("2.0.2.7")>]
+[<assembly: AssemblyFileVersionAttribute("2.0.2.7")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.0.1.7"
+    let [<Literal>] Version = "2.0.2.7"
