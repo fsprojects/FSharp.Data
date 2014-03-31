e89843c56fa0966592eacbb9a2c6c6551efe7e49
Gustavo Guerra <gustavo@codebeside.org>
2014-03-31 15:33:37 +0100

    Buble up exceptions in console tests

--- tests/TestApps/Program.fs
+++ tests/TestApps/Program.fs
@@ -2,12 +2,8 @@
 
 [<EntryPoint>]
 let main _ = 
-    try
-        if System.Reflection.Assembly.GetExecutingAssembly().GetName().Name = "ConsoleApp_4310_PCL7_3310" then
-            printfn "Test disabled until #521 is fixed"
-        else
-            getTestData() |> Async.Ignore |> Async.RunSynchronously
-        0
-    with e ->
-        eprintfn "%s" e.Message
-        -1
+    if System.Reflection.Assembly.GetExecutingAssembly().GetName().Name = "ConsoleApp_4310_PCL7_3310" then
+        printfn "Test disabled until #521 is fixed"
+    else
+        getTestData() |> Async.Ignore |> Async.RunSynchronously
+    0
