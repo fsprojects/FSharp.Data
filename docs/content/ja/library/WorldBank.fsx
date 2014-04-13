73fe8ff5a2cda089643aa14ed09808818e3bb97a
Gustavo Guerra <gustavo@codebeside.org>
2014-04-13 23:21:39 +0100

    Update FSharp.Formatting to get new github icons

--- docs/content/ja/library/WorldBank.fsx
+++ docs/content/ja/library/WorldBank.fsx
@@ -52,7 +52,7 @@ data
 
 *)
 
-#load "../../../../packages/FSharp.Charting.0.90.5/FSharp.Charting.fsx"
+#load "../../../../packages/FSharp.Charting.0.90.6/FSharp.Charting.fsx"
 open FSharp.Charting
 
 data.Countries.``United Kingdom``
