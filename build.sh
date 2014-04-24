4dc5969f24fa4ebcfda1cbe54628199e1cd998c8
Gustavo Guerra <gustavo@codebeside.org>
2014-04-24 12:01:35 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ if [ ! -f packages/FAKE/tools/Fake.exe ]; then
   mono --runtime=v4.0 .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
 fi
 #if [ ! -f packages/SourceLink.Fake/tools/Fake.fsx ]; then
-#  mono --runtime=v4.0 .NuGet/NuGet.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
+#  mono --runtime=v4.0 .nuget/NuGet.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
 #fi
 mono --runtime=v4.0 packages/FAKE/tools/FAKE.exe build.fsx -d:MONO $@
