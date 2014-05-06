1b36c40e57bf527781b9768908ad5170bd1f5ae1
Taylor Wood <taylorwood@me.com>
2014-05-05 21:57:54 -0500

    Make CsvExtensions C# friendly

--- src/Csv/CsvExtensions.fs
+++ src/Csv/CsvExtensions.fs
@@ -1,55 +1,65 @@
 ﻿/// Unsafe extension methods that can be used to work with CsvRow in a less safe, but shorter way.
 /// This module also provides the dynamic operator.
-module FSharp.Data.CsvExtensions
+namespace FSharp.Data
 
 open System
 open System.Globalization
 open FSharp.Data
 open FSharp.Data.Runtime
-  
-type String with
+open System.Runtime.CompilerServices
 
-  member x.AsInteger(?cultureInfo) = 
+[<Extension>]
+type StringExtensions =
+
+  [<Extension>]
+  static member AsInteger(x:String, ?cultureInfo) = 
     let cultureInfo = defaultArg cultureInfo CultureInfo.InvariantCulture
     match TextConversions.AsInteger cultureInfo x with
     | Some i -> i
     | _ -> failwithf "Not an int: %s" x
 
-  member x.AsInteger64(?cultureInfo) = 
+  [<Extension>]
+  static member AsInteger64(x:String, ?cultureInfo) = 
     let cultureInfo = defaultArg cultureInfo CultureInfo.InvariantCulture
     match TextConversions.AsInteger64 cultureInfo x with
     | Some i -> i
     | _ -> failwithf "Not an int64: %s" x
 
-  member x.AsDecimal(?cultureInfo) = 
+  [<Extension>]
+  static member AsDecimal(x:String, ?cultureInfo) =
     let cultureInfo = defaultArg cultureInfo CultureInfo.InvariantCulture
     match TextConversions.AsDecimal cultureInfo x with
     | Some d -> d
     | _ -> failwithf "Not a decimal: %s" x
 
-  member x.AsFloat(?cultureInfo, ?missingValues) = 
+  [<Extension>]
+  static member AsFloat(x:String, ?cultureInfo, ?missingValues) = 
     let cultureInfo = defaultArg cultureInfo CultureInfo.InvariantCulture
     let missingValues = defaultArg missingValues TextConversions.DefaultMissingValues
     match TextConversions.AsFloat missingValues (*useNoneForMissingValues*)false cultureInfo x with
     | Some f -> f
     | _ -> failwithf "Not a float: %s" x
-
-  member x.AsBoolean(?cultureInfo) =
+  
+  [<Extension>]
+  static member AsBoolean(x:String, ?cultureInfo) =
     let cultureInfo = defaultArg cultureInfo CultureInfo.InvariantCulture
     match TextConversions.AsBoolean cultureInfo x with
     | Some b -> b
     | _ -> failwithf "Not a boolean: %s" x
 
-  member x.AsDateTime(?cultureInfo) = 
+  [<Extension>]
+  static member AsDateTime(x:String, ?cultureInfo) = 
     let cultureInfo = defaultArg cultureInfo CultureInfo.InvariantCulture
     match TextConversions.AsDateTime cultureInfo x with 
     | Some d -> d
     | _ -> failwithf "Not a datetime: %s" x
 
-  member x.AsGuid() =
+  [<Extension>]
+  static member AsGuid(x:String) =
     match x |> TextConversions.AsGuid with
     | Some g -> g
     | _ -> failwithf "Not a guid: %s" x
 
-/// Get column of a CsvRow
-let (?) (csvRow:CsvRow) (columnName:string) = csvRow.[columnName]
+module CsvExtensions =
+  /// Get column of a CsvRow
+  let (?) (csvRow:CsvRow) (columnName:string) = csvRow.[columnName]
