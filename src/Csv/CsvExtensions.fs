e330b73390278d31a76a5ec302ebba815c0cdb35
Taylor Wood <taylorwood@me.com>
2014-05-06 07:56:42 -0500

    Optional and DefaultParameterValue attributes for CsvExtensions C# interop

--- src/Csv/CsvExtensions.fs
+++ src/Csv/CsvExtensions.fs
@@ -4,36 +4,37 @@ namespace FSharp.Data
 
 open System
 open System.Globalization
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
 open FSharp.Data
 open FSharp.Data.Runtime
-open System.Runtime.CompilerServices
 
 [<Extension>]
 type CsvExtensions =
 
   [<Extension>]
-  static member AsInteger(x:String, ?cultureInfo) = 
+  static member AsInteger(x:String, [<Optional;DefaultParameterValue(null)>] ?cultureInfo) = 
     let cultureInfo = defaultArg cultureInfo CultureInfo.InvariantCulture
     match TextConversions.AsInteger cultureInfo x with
     | Some i -> i
     | _ -> failwithf "Not an int: %s" x
 
   [<Extension>]
-  static member AsInteger64(x:String, ?cultureInfo) = 
+  static member AsInteger64(x:String, [<Optional;DefaultParameterValue(null)>] ?cultureInfo) = 
     let cultureInfo = defaultArg cultureInfo CultureInfo.InvariantCulture
     match TextConversions.AsInteger64 cultureInfo x with
     | Some i -> i
     | _ -> failwithf "Not an int64: %s" x
 
   [<Extension>]
-  static member AsDecimal(x:String, ?cultureInfo) =
+  static member AsDecimal(x:String, [<Optional;DefaultParameterValue(null)>] ?cultureInfo) =
     let cultureInfo = defaultArg cultureInfo CultureInfo.InvariantCulture
     match TextConversions.AsDecimal cultureInfo x with
     | Some d -> d
     | _ -> failwithf "Not a decimal: %s" x
 
   [<Extension>]
-  static member AsFloat(x:String, ?cultureInfo, ?missingValues) = 
+  static member AsFloat(x:String, [<Optional;DefaultParameterValue(null)>] ?cultureInfo, [<Optional;DefaultParameterValue(null)>] ?missingValues) = 
     let cultureInfo = defaultArg cultureInfo CultureInfo.InvariantCulture
     let missingValues = defaultArg missingValues TextConversions.DefaultMissingValues
     match TextConversions.AsFloat missingValues (*useNoneForMissingValues*)false cultureInfo x with
@@ -41,14 +42,14 @@ type CsvExtensions =
     | _ -> failwithf "Not a float: %s" x
   
   [<Extension>]
-  static member AsBoolean(x:String, ?cultureInfo) =
+  static member AsBoolean(x:String, [<Optional;DefaultParameterValue(null)>] ?cultureInfo) =
     let cultureInfo = defaultArg cultureInfo CultureInfo.InvariantCulture
     match TextConversions.AsBoolean cultureInfo x with
     | Some b -> b
     | _ -> failwithf "Not a boolean: %s" x
 
   [<Extension>]
-  static member AsDateTime(x:String, ?cultureInfo) = 
+  static member AsDateTime(x:String, [<Optional;DefaultParameterValue(null)>] ?cultureInfo) = 
     let cultureInfo = defaultArg cultureInfo CultureInfo.InvariantCulture
     match TextConversions.AsDateTime cultureInfo x with 
     | Some d -> d
--- tests/FSharp.Data.Tests.CSharp/CsvExtensionsTests.cs
+++ tests/FSharp.Data.Tests.CSharp/CsvExtensionsTests.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Globalization;
-using Microsoft.FSharp.Core;
 using NUnit.Framework;
 
 namespace FSharp.Data.Tests.CSharp
@@ -8,13 +6,11 @@ namespace FSharp.Data.Tests.CSharp
     [TestFixture]
     public class CsvExtensionsTests
     {
-        private readonly FSharpOption<CultureInfo> _cultureInfo = FSharpOption<CultureInfo>.Some(CultureInfo.CurrentCulture);
-        
         [Test]
         public void AsInteger_with_valid_integer()
         {
             const string intStr = "123456";
-            int result = intStr.AsInteger(_cultureInfo);
+            int result = intStr.AsInteger();
             Assert.AreEqual(123456, result);
         }
 
@@ -22,7 +18,7 @@ namespace FSharp.Data.Tests.CSharp
         public void AsInteger64_with_valid_integer()
         {
             const string int64Str = "9223372036854775807";
-            long result = int64Str.AsInteger64(_cultureInfo);
+            long result = int64Str.AsInteger64();
             Assert.AreEqual(9223372036854775807, result);
         }
 
@@ -30,7 +26,7 @@ namespace FSharp.Data.Tests.CSharp
         public void AsDecimal_with_valid_decimal()
         {
             const string decimalStr = "123.456";
-            decimal result = decimalStr.AsDecimal(_cultureInfo);
+            decimal result = decimalStr.AsDecimal();
             Assert.AreEqual(123.456, result);
         }
 
@@ -38,7 +34,7 @@ namespace FSharp.Data.Tests.CSharp
         public void AsFloat_with_valid_float()
         {
             const string floatStr = "0.1234567890";
-            double result = floatStr.AsFloat(_cultureInfo, FSharpOption<string[]>.None);
+            double result = floatStr.AsFloat();
             Assert.AreEqual(0.1234567890d, result);
         }
 
@@ -46,7 +42,7 @@ namespace FSharp.Data.Tests.CSharp
         public void AsBoolean_with_valid_boolean()
         {
             const string boolStr = "True";
-            bool result = boolStr.AsBoolean(_cultureInfo);
+            bool result = boolStr.AsBoolean();
             Assert.AreEqual(true, result);
         }
 
@@ -54,7 +50,7 @@ namespace FSharp.Data.Tests.CSharp
         public void AsDateTime_with_valid_date()
         {
             const string dateStr = "4/23/1982";
-            DateTime result = dateStr.AsDateTime(_cultureInfo);
+            DateTime result = dateStr.AsDateTime();
             Assert.AreEqual(new DateTime(1982, 4, 23), result);
         }
 
