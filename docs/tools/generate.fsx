c2c71a971078199caad12b037e0d61a641220de2
Gustavo Guerra <gustavo@codebeside.org>
2014-08-24 16:15:05 +0100

    Update FSharp.Formatting

--- docs/tools/generate.fsx
+++ docs/tools/generate.fsx
@@ -22,8 +22,8 @@ let info =
 // --------------------------------------------------------------------------------------
 
 #I "../../packages/FSharp.Charting.0.90.6/lib/net40"
-#I "../../packages/FSharp.Compiler.Service.0.0.44/lib/net40"
-#I "../../packages/FSharp.Formatting.2.4.8/lib/net40"
+#I "../../packages/FSharp.Compiler.Service.0.0.59/lib/net40"
+#I "../../packages/FSharp.Formatting.2.4.21/lib/net40"
 #I "../../packages/RazorEngine.3.3.0/lib/net40/"
 #r "../../packages/Microsoft.AspNet.Razor.2.0.30506.0/lib/net40/System.Web.Razor.dll"
 #r "../../packages/FAKE/tools/FakeLib.dll"
@@ -60,7 +60,7 @@ let output     = __SOURCE_DIRECTORY__ @@ "../output"
 let files      = __SOURCE_DIRECTORY__ @@ "../files"
 let data       = __SOURCE_DIRECTORY__ @@ "../content/data"
 let templates  = __SOURCE_DIRECTORY__ @@ "templates"
-let formatting = __SOURCE_DIRECTORY__ @@ "../../packages/FSharp.Formatting.2.4.8/"
+let formatting = __SOURCE_DIRECTORY__ @@ "../../packages/FSharp.Formatting.2.4.21/"
 let docTemplate = formatting @@ "templates/docpage.cshtml"
 
 // Where to look for *.cshtml templates (in this order)
--- tests/FSharp.Data.DesignTime.Tests/DocumentationTests.fs
+++ tests/FSharp.Data.DesignTime.Tests/DocumentationTests.fs
@@ -1,8 +1,8 @@
 ﻿#if INTERACTIVE
-#I "../../packages/FSharp.Formatting.2.4.8/lib/net40"
+#I "../../packages/FSharp.Formatting.2.4.21/lib/net40"
 #I "../../packages/RazorEngine.3.3.0/lib/net40/"
 #r "../../packages/Microsoft.AspNet.Razor.2.0.30506.0/lib/net40/System.Web.Razor.dll"
-#r "../../packages/FSharp.Compiler.Service.0.0.44/lib/net40/FSharp.Compiler.Service.dll"
+#r "../../packages/FSharp.Compiler.Service.0.0.59/lib/net40/FSharp.Compiler.Service.dll"
 #r "RazorEngine.dll"
 #r "FSharp.Literate.dll"
 #r "FSharp.CodeFormat.dll"
