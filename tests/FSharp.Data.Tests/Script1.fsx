96505b7604384cfe9a28e7bb6b18f2412224a2e0
Colin <colinbul@googlemail.com>
2015-01-27 17:45:13 +0000

    Adding tranisent test script

--- tests/FSharp.Data.Tests/Script1.fsx
+++ tests/FSharp.Data.Tests/Script1.fsx
@@ -17,45 +17,48 @@ let table = HtmlProvider<"""<html>
                     </table>
                 </body>
             </html>""">.GetSample().Tables.Table1
-let distance = table |> Seq.map (fun x -> x.Distance)
+let distance = table |> Seq.map (fun x -> x.Distance) |> Seq.toList
 
-let table1 = HtmlProvider<"""<html>
-                <body>
-                    <table>
-                        <tr><td>Date</td><td>Distance (m)</td><td>Time (s)</td><td>Column 3</td><td>Column 4</td></tr>
-                        <tr>
-                            <td>01/01/2013 12:00</td>
-                            <td>2</td><td>30.5</td>
-                            <td>
-                                <div itemscope itemtype ="http://schema.org/Movie">
-                                  <h1 itemprop="name">Avatar</h1>
-                                  <div itemprop="director" itemscope itemtype="http://schema.org/Person">
-                                  Director: <span itemprop="name">James Cameron</span> (born <span itemprop="birthDate">August 16, 1954 </span>)
-                                  </div>
-                                  <span itemprop="genre">Science fiction</span>
-                                  <a href="../movies/avatar-theatrical-trailer.html" itemprop="trailer">Trailer</a>
-                                </div>
-                            </td>
-                            <td>2</td>
-                        </tr>
-                        <tr>
-                            <td>01/01/2013 12:00</td>
-                            <td>1.5</td>
-                            <td>30.5</td>
-                            <td>
-                                <div itemscope itemtype ="http://schema.org/Movie">
-                                  <h1 itemprop="name">Alien</h1>
-                                  <div itemprop="director" itemscope itemtype="http://schema.org/Person">
-                                  Director: <span itemprop="name">James Cameron</span> (born <span itemprop="birthDate">August 16, 1954 </span>)
-                                  </div>
-                                  <span itemprop="genre">Science fiction</span>
-                                  <a href="../movies/avatar-theatrical-trailer.html" itemprop="trailer">Trailer</a>
-                                </div>
-                            </td>
-                            <td>2</td>
-                        </tr>
-                    </table>
-                </body>
-            </html>""">.GetSample().Tables.Table1
+let [<Literal>] data= 
+    """<html>
+           <body>
+               <table>
+                   <tr><td>Date</td><td>Distance (m)</td><td>Time (s)</td><td>Column 3</td><td>Column 4</td></tr>
+                   <tr>
+                       <td>01/01/2013 12:00</td>
+                       <td>2</td><td>30.5</td>
+                       <td>
+                           <div itemscope itemtype ="http://schema.org/Movie">
+                             <h1 itemprop="name">Avatar</h1>
+                             <div itemprop="director" itemscope itemtype="http://schema.org/Person">
+                             Director: <span itemprop="name">James Cameron</span> (born <span itemprop="birthDate">August 16, 1954 </span>)
+                             </div>
+                             <span itemprop="genre">Science fiction</span>
+                             <a href="../movies/avatar-theatrical-trailer.html" itemprop="trailer">Trailer</a>
+                           </div>
+                       </td>
+                       <td>2</td>
+                   </tr>
+                   <tr>
+                       <td>01/01/2013 12:00</td>
+                       <td>1.5</td>
+                       <td>30.5</td>
+                       <td>
+                           <div itemscope itemtype ="http://schema.org/Movie">
+                             <h1 itemprop="name">Alien</h1>
+                             <div itemprop="director" itemscope itemtype="http://schema.org/Person">
+                             Director: <span itemprop="name">James Cameron</span> (born <span itemprop="birthDate">August 16, 1954 </span>)
+                             </div>
+                             <span itemprop="genre">Science fiction</span>
+                             <a href="../movies/avatar-theatrical-trailer.html" itemprop="trailer">Trailer</a>
+                           </div>
+                       </td>
+                       <td>2</td>
+                   </tr>
+               </table>
+           </body>
+       </html>"""
 
-let movieName = table1 |> Seq.map (fun x -> x.``Column 3``.name)
+let table1 = HtmlProvider<data>.GetSample().Tables.Table1
+
+let movieName = table1 |> Seq.map (fun x -> x.``Column 3``.genre) |> Seq.toList
