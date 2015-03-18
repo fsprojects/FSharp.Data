fe540d7b46e98055427767cd9b2a076d460a0c44
Colin <colinbul@googlemail.com>
2015-03-18 09:45:13 +0000

    Added FixedWidthReader tests

---
+++ tests/FSharp.Data.Tests/FixedWidthReader.fs
@@ -0,0 +1,68 @@
+﻿module FSharp.Data.Tests.FixedWidthReader
+
+#if INTERACTIVE
+#r "../../bin/FSharp.Data.dll"
+#r "../../packages/NUnit/lib/nunit.framework.dll"
+#load "../Common/FsUnit.fs"
+#endif
+
+open NUnit.Framework
+open FsUnit
+open System.IO
+open FSharp.Data.Runtime.FixedWidthReader
+
+[<Test>]
+let ``Line with quotes parsed`` () = 
+  let sr = new StringReader("123  \"456\"")
+  let actual = readFixedWidthFile sr '"'|> Seq.map fst |> Array.ofSeq
+  let expected = [|[|"123"; "456"|]|]
+  actual |> should equal expected
+
+[<Test>]
+let ``Quotes can be escaped with double quote`` () = 
+  let sr = new StringReader("123  \"45\"\"6\"")
+  let actual = readFixedWidthFile sr '"'|> Seq.map fst |> Array.ofSeq
+  let expected = [|[|"123"; "45\"6"|]|]
+  actual |> should equal expected
+
+[<Test>]
+let ``Line with custom quotes parsed`` () = 
+  let sr = new StringReader("123  '45,6'")
+  let actual = readFixedWidthFile sr '''|> Seq.map fst |> Array.ofSeq
+  let expected = [|[|"123"; "45,6"|]|]
+  actual |> should equal expected
+
+[<Test>]
+let ``Custom quotes can be escaped with double custom quote`` () = 
+  let sr = new StringReader("123  '45''6'")
+  let actual = readFixedWidthFile sr '''|> Seq.map fst |> Array.ofSeq
+  let expected = [|[|"123"; "45'6"|]|]
+  actual |> should equal expected
+
+[<Test>]
+let ``Quoted new line does not start new row`` () = 
+  let sr = new StringReader("123  \"45\n6\"")
+  let actual = readFixedWidthFile sr '"'|> Seq.map fst |> Array.ofSeq
+  let expected = [|[|"123"; "45\n6"|]|]
+  actual |> should equal expected
+
+[<Test>]
+let ``Quoted separator does not start new column`` () = 
+  let sr = new StringReader("123  \"45  6\"")
+  let actual = readFixedWidthFile sr '"'|> Seq.map fst |> Array.ofSeq
+  let expected = [|[|"123"; "45  6"|]|]
+  actual |> should equal expected
+
+[<Test>]
+let ``Blank lines are ignored and parsed correctly`` () = 
+  let sr = new StringReader("\n\r12  34\r\n\r\n56  78\n90  10\n")
+  let actual = readFixedWidthFile sr '"'|> Seq.map fst |> Array.ofSeq
+  let expected = [| [|"12"; "34" |]; [|"56"; "78"|]; [|"90"; "10" |]|]
+  actual |> should equal expected
+
+[<Test>]
+let ``Quoted strings parsed correctly`` () = 
+  let sr = new StringReader("\n\r12  \"a\n\rb\"\r\n\"123\"  \"\"\"hello\"\" world\"\n\r")
+  let actual = readFixedWidthFile sr '"'|> Seq.map fst |> Array.ofSeq
+  let expected = [|[|"12"; "a\n\rb"|]; [|"123"; "\"hello\" world"|]|]
+  actual |> should equal expected
