d4296eb4bda26f68f3575285f3b80e2cd6dd79f1
rflechner <rflechner@romcyber.com>
2015-10-21 19:40:47 +0200

    add table listing all JQuery selectors and their status

--- docs/content/library/HtmlCssSelectors.fsx
+++ docs/content/library/HtmlCssSelectors.fsx
@@ -302,8 +302,80 @@ Find all files uploaders.
 let file = htmlForm.CssSelect ":file"
 (*** include-value:file ***)
 
+(**
 
+## Implemented and missing features
 
+Basic CSS selectors are implemented, but some JQuery selectors are missing
 
+This table lists all JQuery selectors and their status
 
+Selector name|Status|specification
+|-|:-:|-:
+*All Selector *|` TODO `|[specification](http://api.jquery.com/all-selector/)|
+*:animated Selector*|` not possible `|[specification](http://api.jquery.com/animated-selector/)|
+*Attribute Contains Prefix Selector*|` implemented `|[specification](http://api.jquery.com/attribute-contains-prefix-selector/)|
+*Attribute Contains Selector *|` implemented `|[specification](http://api.jquery.com/attribute-contains-selector/)|
+*Attribute Contains Word Selector*|` implemented `|[specification](http://api.jquery.com/attribute-contains-word-selector/)|
+*Attribute Ends With Selector *|` implemented `|[specification](http://api.jquery.com/attribute-ends-with-selector/)|
+*Attribute Equals Selector *| ` implemented `|[specification](http://api.jquery.com/attribute-equals-selector/)|
+*Attribute Not Equal Selector*|` implemented `|[specification](http://api.jquery.com/attribute-not-equal-selector/)|
+*Attribute Starts With Selector *|` implemented `|[specification](http://api.jquery.com/attribute-starts-with-selector/)|
+*:button Selector *|` implemented `|[specification](http://api.jquery.com/button-selector/)|
+*:checkbox Selector*|` implemented `|[specification](http://api.jquery.com/checkbox-selector/)|
+*:checked Selector*|` implemented `|[specification](http://api.jquery.com/checked-selector/)|
+*Child Selector (“parent > child”)*|` implemented `|[specification](http://api.jquery.com/child-selector/)|
+*Class Selector (“.class”)*|` implemented `|[specification](http://api.jquery.com/class-selector/)|
+*:contains() Selector*|` TODO `|[specification](http://api.jquery.com/contains-selector/)|
+*Descendant Selector (“ancestor descendant”)*|` implemented `|[specification](http://api.jquery.com/descendant-selector/)|
+*:disabled Selector*|` implemented `|[specification](http://api.jquery.com/disabled-selector/)|
+*Element Selector (“element”)*|` implemented `|[specification](http://api.jquery.com/element-selector/)|
+*:empty Selector*|` implemented `|[specification](http://api.jquery.com/empty-selector/)|
+*:enabled Selector*|` implemented `|[specification](http://api.jquery.com/enabled-selector/)|
+*:eq() Selector*|` TODO `|[specification](http://api.jquery.com/eq-selector/)|
+*:even Selector*|` implemented `|[specification](http://api.jquery.com/even-selector/)|
+*:file Selector*|` implemented `|[specification](http://api.jquery.com/file-selector/)|
+*:first-child Selector*|` TODO `|[specification](http://api.jquery.com/first-child-selector/)|
+*:first-of-type Selector*|` TODO  `|[specification](http://api.jquery.com/first-of-type-selector/)|
+*:first Selector*|` TODO `|[specification](http://api.jquery.com/first-selector/)|
+*:focus Selector*|` not possible `|[specification](http://api.jquery.com/focus-selector/)|
+*:gt() Selector*|` TODO `|[specification](http://api.jquery.com/gt-selector/)|
+*Has Attribute Selector [name]*|` implemented `|[specification](http://api.jquery.com/has-attribute-selector/)|
+*:has() Selector*|` TODO `|[specification](http://api.jquery.com/has-selector/)|
+*:header Selector*|` TODO `|[specification](http://api.jquery.com/header-selector/)|
+*:hidden Selector*|` implemented `|[specification](http://api.jquery.com/hidden-selector/)|
+*ID Selector (“#id”)*|` implemented `|[specification](http://api.jquery.com/id-selector/)|
+*:image Selector*|` implemented `|[specification](http://api.jquery.com/image-selector/)|
+*:input Selector*|` implemented `|[specification](http://api.jquery.com/input-selector/)|
+*:lang() Selector*|` TODO `|[specification](http://api.jquery.com/lang-selector/)|
+*:last-child Selector*|` TODO `|[specification](http://api.jquery.com/last-child-selector/)|
+*:last-of-type Selector*|` TODO `|[specification](http://api.jquery.com/last-of-type-selector/)|
+*:last Selector*|` TODO `|[specification](http://api.jquery.com/last-selector/)|
+*:lt() Selector*|` TODO `|[specification](http://api.jquery.com/lt-selector/)|
+*Multiple Attribute Selector [name=”value”][name2=”value2″]*|` implemented `|[specification](http://api.jquery.com/multiple-attribute-selector/)|
+*Multiple Selector (“selector1, selector2, selectorN”)*|` TODO `|[specification](http://api.jquery.com/multiple-selector/)|
+*Next Adjacent Selector (“prev + next”)*|` TODO `|[specification](http://api.jquery.com/next-adjacent-selector/)|
+*Next Siblings Selector (“prev ~ siblings”)*|` TODO `|[specification](http://api.jquery.com/next-siblings-selector/)|
+*:not() Selector*|` TODO `|[specification](http://api.jquery.com/not-selector/)|
+*:nth-child() Selector*|` TODO `|[specification](http://api.jquery.com/nth-child-selector/)|
+*:nth-last-child() Selector*|` TODO `|[specification](http://api.jquery.com/nth-last-child-selector/)|
+*:nth-last-of-type() Selector*|` TODO `|[specification](http://api.jquery.com/nth-last-of-type-selector/)|
+*:nth-of-type() Selector*|` TODO `|[specification](http://api.jquery.com/nth-of-type-selector/)|
+*:odd Selector*|` implemented `|[specification](http://api.jquery.com/odd-selector/)|
+*:only-child Selector*|` TODO `|[specification](http://api.jquery.com/only-child-selector/)|
+*:only-of-type Selector*|` TODO `|[specification](http://api.jquery.com/only-of-type-selector/)|
+*:parent Selector*|` TODO `|[specification](http://api.jquery.com/parent-selector/)|
+*:password Selector*|` implemented `|[specification](http://api.jquery.com/password-selector/)|
+*:radio Selector*|` implemented `|[specification](http://api.jquery.com/radio-selector/)|
+*:reset Selector*|` not possible `|[specification](http://api.jquery.com/reset-selector/)|
+*:root Selector*|` useless[1] `|[specification](http://api.jquery.com/root-selector/)|
+*:selected Selector*|` implemented `|[specification](http://api.jquery.com/selected-selector/)|
+*:submit Selector*|` implemented `|[specification](http://api.jquery.com/submit-selector/)|
+*:target Selector*|` not possible `|[specification](http://api.jquery.com/target-selector/)|
+*:text Selector*|` implemented `|[specification](http://api.jquery.com/text-selector/)|
+*:visible Selector*|` not possible `|[specification](http://api.jquery.com/visible-selector/)|
+
+[1] :root Selector seems to be useless in our case because with the HTML parser the root is always the html node.
+
+*)
 
