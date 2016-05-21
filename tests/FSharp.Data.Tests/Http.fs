cdf46943039c497d7ea3e5eb3b0ce829930d12fa
Gustavo Guerra <gustavo@codebeside.org>
2016-05-21 17:58:20 +0100

    Handle cookies with commas in their value correctly (closes #904)

--- tests/FSharp.Data.Tests/Http.fs
+++ tests/FSharp.Data.Tests/Http.fs
@@ -46,3 +46,14 @@ let ``Two custom header with different names don't throw an exception`` () =
 let ``A request with an invalid url throws an exception`` () =
     (fun() -> Http.Request "www.google.com" |> ignore) |> should throw typeof<UriFormatException>
 
+[<Test>]
+let ``Cookies with commas are parsed correctly`` () =
+    let uri = Uri "http://www.nasdaq.com/symbol/ibm/dividend-history"
+    let cookieHeader = "selectedsymboltype=IBM,COMMON STOCK,NYSE; domain=.nasdaq.com; expires=Sun, 21-May-2017 15:29:03 GMT; path=/,selectedsymbolindustry=IBM,technology; domain=.nasdaq.com; expires=Sun, 21-May-2017 15:29:03 GMT; path=/,NSC_W.TJUFEFGFOEFS.OBTEBR.80=ffffffffc3a08e3045525d5f4f58455e445a4a423660;expires=Sat, 21-May-2016 15:39:03 GMT;path=/;httponly"
+    let cookies = 
+        CookieHandling.getAllCookiesFromHeader cookieHeader uri 
+        |> Array.map (snd >> (fun c -> c.Name, c.Value))
+    cookies |> should equal
+        [| "selectedsymboltype", "IBM,COMMON STOCK,NYSE"
+           "selectedsymbolindustry", "IBM,technology"
+           "NSC_W.TJUFEFGFOEFS.OBTEBR.80", "ffffffffc3a08e3045525d5f4f58455e445a4a423660" |]
