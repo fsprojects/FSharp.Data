5b6dc4034249560a0f8fcb6bcd075d824fcf6367
Gustavo Guerra <gustavo@codebeside.org>
2016-07-24 18:08:51 +0100

    Fix failing tests

--- src/AssemblyInfo.DesignTime.fs
+++ src/AssemblyInfo.DesignTime.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Data.DesignTime")>]
 [<assembly: AssemblyProductAttribute("FSharp.Data")>]
 [<assembly: AssemblyDescriptionAttribute("Library of F# type providers and data access tools")>]
-[<assembly: AssemblyVersionAttribute("2.3.1.0")>]
-[<assembly: AssemblyFileVersionAttribute("2.3.1.0")>]
+[<assembly: AssemblyVersionAttribute("2.3.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("2.3.2.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.3.1.0"
-    let [<Literal>] InformationalVersion = "2.3.1.0"
+    let [<Literal>] Version = "2.3.2.0"
+    let [<Literal>] InformationalVersion = "2.3.2.0"
