83a8ddfda9f525315e069ab4bb36b5f0ab40def4
zpbappi <zpbappi@gmail.com>
2016-08-16 15:33:04 +1000

    fix for issue #978: replacing null with empty string when writing

--- tests/FSharp.Data.Tests/CsvProvider.fs
+++ tests/FSharp.Data.Tests/CsvProvider.fs
@@ -481,3 +481,76 @@ let ``Fields with quotes should be quoted and escaped when saved``() =
     let roundTripped = SimpleWithStrCsv.Parse(csv.SaveToString())
     let rowRoundTripped = roundTripped.Rows |> Seq.exactlyOne
     rowRoundTripped |> should equal rowWithQuoteInField
+
+
+
+type MappingType = CsvProvider<"IndicatorName, CountryName, Type, Code", 
+                                                        Schema = "IndicatorName (string), CountryName (string), Type (string), Code (string)", 
+                                                        Encoding="windows-1252", 
+                                                        HasHeaders=true, 
+                                                        SkipRows=0>
+type WithName = {
+    Name: string
+}
+
+type Mapping = {
+    Indicator: WithName
+    Country: WithName
+    RelationshipType: WithName
+    Code: string
+}
+
+[<Test>]
+let ``Having null in a cell should not fail saving to string (issue#978)`` () =
+    
+    let Stringify (mappings:seq<Mapping>) =
+                    let rows = mappings |> Seq.map(fun x -> MappingType.Row(x.Indicator.Name, x.Country.Name, x.RelationshipType.ToString(), x.Code )) 
+                    let output = new MappingType( rows )
+                    output.SaveToString()
+
+    
+    let mutable counter = 1
+    let data = seq {
+        yield {
+            Indicator = {Name = "Ind-" + counter.ToString() }
+            Country = {Name = "Cnt-" + counter.ToString() }
+            RelationshipType = {Name = "RT-" + counter.ToString() }
+            Code = "code" + counter.ToString()
+        }
+
+        counter <- counter + 1
+        yield {
+            Indicator = {Name = "Ind-" + counter.ToString() }
+            Country = {Name = "Cnt-" + counter.ToString() }
+            RelationshipType = {Name = "RT-" + counter.ToString() }
+            Code = ""
+        }
+
+        counter <- counter + 1
+        yield {
+            Indicator = {Name = "Ind-" + counter.ToString() }
+            Country = {Name = "Cnt-" + counter.ToString() }
+            RelationshipType = {Name = "RT-" + counter.ToString() }
+            Code = null
+        }
+        
+        counter <- counter + 1
+        yield {
+            Indicator = {Name = "Ind-" + counter.ToString() }
+            Country = {Name = "Cnt-" + counter.ToString() }
+            RelationshipType = {Name = "RT-" + counter.ToString() }
+            Code = "null"
+        }
+
+        counter <- counter + 1
+        yield {
+            Indicator = {Name = "Ind-" + counter.ToString() }
+            Country = {Name = "Cnt-" + counter.ToString() }
+            RelationshipType = {Name = "RT-" + counter.ToString() }
+            Code = "[null]"
+        }
+    }
+
+    data
+    |> Stringify 
+    |> ignore
