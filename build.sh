8f912b756f86546fd1ddf88ee0f101c752f5768a
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-04-11 08:51:14 +0200

    Use Paket in Magic Mode

--- build.sh
+++ build.sh
@@ -3,12 +3,6 @@ if test "$OS" = "Windows_NT"
 then
   # use .Net
 
-  .paket/paket.bootstrapper.exe prerelease
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-
   .paket/paket.exe restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
@@ -18,11 +12,6 @@ then
   packages/FAKE/tools/FAKE.exe $@ --fsiargs build.fsx 
 else
   #!/bin/bash
-  mono .paket/paket.bootstrapper.exe
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
 
   mono .paket/paket.exe restore
   exit_code=$?
