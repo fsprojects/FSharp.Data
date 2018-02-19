919b8976fdcc57e1e35c10c59f555275aed9c274
Gustavo Guerra <guguer@codebeside.org>
2018-02-19 11:46:08 +0000

    Bump version to 2.4.5

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Data")>]
 [<assembly: AssemblyProductAttribute("FSharp.Data")>]
 [<assembly: AssemblyDescriptionAttribute("Library of F# type providers and data access tools")>]
-[<assembly: AssemblyVersionAttribute("2.4.4.0")>]
-[<assembly: AssemblyFileVersionAttribute("2.4.4.0")>]
+[<assembly: AssemblyVersionAttribute("2.4.5.0")>]
+[<assembly: AssemblyFileVersionAttribute("2.4.5.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FSharp.Data"
     let [<Literal>] AssemblyProduct = "FSharp.Data"
     let [<Literal>] AssemblyDescription = "Library of F# type providers and data access tools"
-    let [<Literal>] AssemblyVersion = "2.4.4.0"
-    let [<Literal>] AssemblyFileVersion = "2.4.4.0"
+    let [<Literal>] AssemblyVersion = "2.4.5.0"
+    let [<Literal>] AssemblyFileVersion = "2.4.5.0"
