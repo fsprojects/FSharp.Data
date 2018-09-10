4b0a2a16aa97348e74c4488119415bf445b02fb8
Zhenyong Zhu <zyzhu@users.noreply.github.com>
2018-09-10 10:39:44 -0400

    updated path for fsharp.charting.fsx

--- docs/content/library/HtmlProvider.fsx
+++ docs/content/library/HtmlProvider.fsx
@@ -106,7 +106,7 @@ let stats =
       k, xs |> Seq.sumBy (fun x -> x.Downloads))
 
 // Load the FSharp.Charting library
-#load "../../../packages/test/FSharp.Charting/lib/net45/FSharp.Charting.fsx"
+#load "../../../packages/test/FSharp.Charting/FSharp.Charting.fsx"
 open FSharp.Charting
 
 // Visualize the package stats
