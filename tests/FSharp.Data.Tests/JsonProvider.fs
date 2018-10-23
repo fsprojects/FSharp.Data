48054eaa3e8d27ceac0178c4f665fa1ffa62567f
Ryan Riley <ryan@tachyus.com>
2018-10-23 14:37:48 -0500

    added complex test

--- tests/FSharp.Data.Tests/JsonProvider.fs
+++ tests/FSharp.Data.Tests/JsonProvider.fs
@@ -749,3 +749,10 @@ let ``Getting field with numeric array should infer double from exponential``() 
 let ``Getting field with numeric array should infer double from exponential with decimal``() =
     let json = JsonProvider<""" { "a":[1.0e0, 1.0e0] } """>.GetSample()
     json.A |> should equal [|1.0;1.0|]
+
+[<Test>]
+let ``Getting fields with numeric array should infer correct types``() =
+    let json = JsonProvider<""" { "a":[1, 1], "b":[1.0e0, 1.0e0], "c":[1.0, 1.0] } """>.GetSample()
+    json.A |> should equal [|1;1|]
+    json.B |> should equal [|1.0;1.0|]
+    json.C |> should equal [|1.0m;1.0m|]
