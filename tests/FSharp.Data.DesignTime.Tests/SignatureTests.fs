3bcbb48532c36f28643841bf05af2d94ea531ec8
cartermp <pcarter@fastmail.com>
2021-03-05 18:14:28 -0800

    Update tests

--- tests/FSharp.Data.DesignTime.Tests/SignatureTests.fs
+++ tests/FSharp.Data.DesignTime.Tests/SignatureTests.fs
@@ -28,40 +28,28 @@ let expectedDirectory = sourceDirectory ++ "expected"
 
 let resolutionFolder = sourceDirectory ++ ".." ++ "FSharp.Data.Tests" ++ "Data"
 let assemblyName = "FSharp.Data.dll"
-let netstandard2RuntimeAssembly = sourceDirectory ++ ".." ++ ".." ++ "bin" ++ "lib" ++ "netstandard2.0" ++ assemblyName
+let netstandard2RuntimeAssembly = sourceDirectory ++ ".." ++ ".." ++ "src" ++ "FSharp.Data" ++ "bin" ++ "Release" ++ "netstandard2.0" ++ assemblyName
 
 let getRuntimeRefs platform = TypeProviderInstantiation.GetRuntimeAssemblyRefs platform
 
-let generateAllExpected() =
-    if not <| Directory.Exists expectedDirectory then
-        Directory.CreateDirectory expectedDirectory |> ignore
-    for (sample, testCase) in testCasesTuple do
-        try
-            let assemblyRefs = getRuntimeRefs NetStandard20
-            testCase.Dump (resolutionFolder, expectedDirectory, netstandard2RuntimeAssembly, assemblyRefs, signatureOnly=false, ignoreOutput=false)
-            |> ignore
-        with e ->
-            raise(new Exception(sprintf "Failed generating: %s" sample, e))
-
 let normalize (str:string) =
-  str.Replace("\r\n", "\n").Replace("\r", "\n").Replace("@\"<RESOLUTION_FOLDER>\"", "\"<RESOLUTION_FOLDER>\"")
+    str.Replace("\r\n", "\n").Replace("\r", "\n").Replace("@\"<RESOLUTION_FOLDER>\"", "\"<RESOLUTION_FOLDER>\"")
 
 [<Test>]
 [<TestCaseSource "testCases">]
 let ``Validate signature didn't change `` (testCase:TypeProviderInstantiation) =
-    let path = testCase.ExpectedPath expectedDirectory
-    let expected = path |> File.ReadAllText |> normalize
-    let assemblyRefs = getRuntimeRefs NetStandard20
-    printfn "assemblyRefs = %A" assemblyRefs
-    let outputRaw = testCase.Dump (resolutionFolder, "", netstandard2RuntimeAssembly, assemblyRefs, signatureOnly=false, ignoreOutput=false)
-    let output = outputRaw |> normalize
-    if output <> expected then
-        printfn "Obtained Signature:\n%s" outputRaw
-        File.WriteAllText(testCase.ExpectedPath expectedDirectory + ".obtained", outputRaw)
-    output |> should equal expected
+   let path = testCase.ExpectedPath expectedDirectory
+   let expected = path |> File.ReadAllText |> normalize
+   let assemblyRefs = getRuntimeRefs NetStandard20
+   printfn "assemblyRefs = %A" assemblyRefs
+   let outputRaw = testCase.Dump (resolutionFolder, "", netstandard2RuntimeAssembly, assemblyRefs, signatureOnly=false, ignoreOutput=false)
+   let output = outputRaw |> normalize
+   if output <> expected then
+       printfn "Obtained Signature:\n%s" outputRaw
+       File.WriteAllText(testCase.ExpectedPath expectedDirectory + ".obtained", outputRaw)
+   output |> should equal expected
 
 [<Test>]
 [<TestCaseSource "testCases">]
 let ``Generating expressions works in netstandard2.0 `` (testCase:TypeProviderInstantiation) =
     testCase.Dump(resolutionFolder, "", netstandard2RuntimeAssembly, (getRuntimeRefs NetStandard20), signatureOnly=false, ignoreOutput=true) |> ignore
-
