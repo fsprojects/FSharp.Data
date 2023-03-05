f1115e3da741e6ecc2dfb9c5d47eb9d6cc361bf7
Melvyn Laïly <melvyn.laily@gmail.com>
2023-03-05 17:23:41 +0100

    Do not fail the build if the WorldBank api is offline

--- tests/FSharp.Data.Tests/WorldBankProvider.fs
+++ tests/FSharp.Data.Tests/WorldBankProvider.fs
@@ -1,4 +1,4 @@
-﻿module FSharp.Data.Tests.WorldBankProvider
+module FSharp.Data.Tests.WorldBankProvider
 
 open System
 open System.Net
@@ -13,7 +13,14 @@ let data = WorldBankData.GetDataContext()
 
 [<Test>]
 let ``Should not throw exception for missing year in indicator``() =
-    data.Countries.``United Kingdom``.Indicators.``GNI growth (annual %)``.[1900] |> should equal Double.NaN
-    data.Countries.``United Kingdom``.Indicators.``GNI growth (annual %)``.[2012] |> should not' (equal Double.NaN)
-    data.Countries.``United Kingdom``.Indicators.``GNI growth (annual %)``.TryGetValueAt 1900 |> should equal None
-    data.Countries.``United Kingdom``.Indicators.``GNI growth (annual %)``.TryGetValueAt 2012 |> should not' (equal None)
+    try
+        data.Countries.``United Kingdom``.Indicators.``GNI growth (annual %)``.[1900] |> should equal Double.NaN
+        data.Countries.``United Kingdom``.Indicators.``GNI growth (annual %)``.[2012] |> should not' (equal Double.NaN)
+        data.Countries.``United Kingdom``.Indicators.``GNI growth (annual %)``.TryGetValueAt 1900 |> should equal None
+        data.Countries.``United Kingdom``.Indicators.``GNI growth (annual %)``.TryGetValueAt 2012 |> should not' (equal None)
+    with ex
+        when ex.ToString().Contains("The server has encountered an error which prevents it from fulfilling your request. Please contact the system administrator")
+        || ex.ToString().Contains("Timeout exceeded while getting response")
+        || ex.ToString().Contains("504 Gateway Time-out") ->
+            Assert.Inconclusive($"Worldbank api is having issues: {ex}")
+
